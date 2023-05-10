5197a4ea307a24d56a846dcabc16c583ee6eda93
jaga172023 <jaga.101602@gmail.com>
2023-05-10 13:06:43 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const inputEl = document.getElementById("inputVal")
 const outputEl = document.getElementById("outputVal")
 const inputValEl = document.getElementById("inputCurrency")
-const outputValEl = document.getElementById("outputVal")
+const outputValEl = document.getElementById("outputCurrency")
 const exRateEl = document.getElementById("exRate")
 
-function getInput() {
+getResult1();
+
+function getResult1() {
     // console.log("Value changed!!");
     // console.log(inputEl.value);
 
     fetch(`https://v6.exchangerate-api.com/v6/3be6a850465ee29e7e986a13/latest/${inputEl.value}`
     )
+    
         .then((res)=>res.json())
         .then((data)=> {
-            const rate = data.conversion_rate[outputEl.value];
-            console.log(rate);
+            const rate = data.conversion_rates[outputEl.value];
+            // console.log(data);
+            // console.log(rate);
+
+            exRateEl.innerText = `1 ${inputEl.value}  =  ${rate} ${outputEl.value}`
+            
+            outputValEl.value = (inputValEl.value * rate).toFixed(2);
         });
+        
 
 }
 
@@ -26,11 +35,9 @@ function getOutput() {
 function getInputVal() {
     // console.log("Input Received !!");
     // console.log(inputValEl.value);
-
-
 }
 
 
-inputEl.addEventListener("change", getInput)
-outputEl.addEventListener("change", getOutput)
-inputValEl.addEventListener("input", getInputVal)
+inputEl.addEventListener("change", getResult1)
+outputEl.addEventListener("change", getResult1)
+inputValEl.addEventListener("input", getResult1)
